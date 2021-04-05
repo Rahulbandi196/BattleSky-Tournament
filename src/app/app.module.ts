5b0bae7f2c7f20beeb158f61b981eb09a755ac15
Rahulbandi196 <rahulvbandi@gmail.com>
2021-04-04 22:25:56 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    AuthModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { AppService } from '../app.service';
+import { ReturnStatement } from '@angular/compiler';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards: any = [];
+  cardsForHandset: any = [];
+  cardsForWeb: any = [];
+
+  isHandset: boolean = false;
+  isHandsetObserver: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return true;
+      }
+        return false;
+    })
+  );
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public appService: AppService) { }
+
+  ngOnInit() {
+    this.isHandsetObserver.subscribe(currentObserverValue => {
+      this.isHandset = currentObserverValue;
+      this.loadCards();
+    });
+
+    this.appService.getDeals().subscribe(
+      response => {
+        this.cardsForHandset = response.handsetCards;
+        this.cardsForWeb = response.webCards;
+        this.loadCards();
+
+      },
+      error => {
+
+      }
+    );
+  }
+  loadCards() {
+    this.cards = this.isHandset ? this.cardsForHandset:this.cardsForWeb;
+  }
+
+  getImage(imageName:string):string {
+    return 'url('+'http://localhost:3000/images/'+ imageName +'.jpg' + ')';
+
+  }
+}
+
